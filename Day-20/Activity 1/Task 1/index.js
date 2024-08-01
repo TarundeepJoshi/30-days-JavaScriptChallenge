5ee2787aa4b9945591807a51077824bf4f12fcee
Tarundeep Joshi <tarundeepjoshi0002@gmail.com>
2024-08-01 18:07:29 +0530

    Day-20 Challenge Done

---
+++ Day-20/Activity 1/Task 1/index.js
@@ -0,0 +1,15 @@
+/**
+ * Day 20 - LocalStorage and SessionStorage
+ *
+ * Activity 1 - Understanding LocalStorage
+ *  - Task 1: Write a script to save a string value to `localStorage` and retrieve it. Log the retrieved value.
+ */
+
+// Save a string value to `localStorage`
+localStorage.setItem("name", "Tarundeep Joshi");
+
+// Retrieve the saved value from `localStorage`
+const name = localStorage.getItem("name");
+
+// Log the retrieved value
+console.log(name); // Output: Tarundeep Joshi
---
+++ Day-20/Activity 3/Task 6/index.js
@@ -0,0 +1,25 @@
+/**
+ * Day 20 - LocalStorage and SessionStorage
+ *
+ * Activity 3 - Understanding LocalStorage
+ *  - Task 6: Write a script to save an object to `sessionStorage` by converting it to a JSON string. Retrieve and parse the object, then log it.
+ */
+
+// Save an object to `sessionStorage` by converting it to a JSON string
+const user = {
+  name: "Tarundeep Joshi",
+  email: "joshi@gmail.com",
+  age: 20,
+};
+
+// Convert the object to a JSON string and save it to `sessionStorage`
+sessionStorage.setItem("user", JSON.stringify(user));
+
+// Retrieve the saved object from `sessionStorage`
+const retrievedSavedUser = sessionStorage.getItem("user");
+
+// Parse the retrieved JSON string to an object
+const savedUser = JSON.parse(retrievedSavedUser);
+
+// Log the parsed object
+console.log(savedUser); // Output: { name: 'Tarundeep Joshi', email: 'joshi@gmail.com', age: 20 }
